6c40e4435616ed7a2e5db8a155a49bb8c3f7821e
iuMIK <macbookair@iuMIK.local>
2026-01-03 15:35:21 +0700

    fixed issue with import

--- src/app/resources/admin/admin.routes.ts
+++ src/app/resources/admin/admin.routes.ts
@@ -5,7 +5,7 @@ import { TeacherModule } from './a2-teachers/teachers.module';
 
 import { DepartmentModule } from './a4-departments/department.module';
 import { AdminModule } from './admin.module';
-import { HodModule } from './a3-HODs/hods.module';
+import { HodModule } from './a3-hods/hods.module';
 
 export const adminRoutes: Routes = [
   {
